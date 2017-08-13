ab3f633a851424da8bd2ef6a5600c2e6fd7ebf0e
Vadim Vzorov <vzblazer@gmail.com>
2017-08-14 00:22:23 +0200

    filter design 1

--- src/app/filter-data.ts
+++ src/app/filter-data.ts
@@ -1,7 +1,7 @@
 import { Filter } from './filter';
 
 export const FILTERS: Filter[] = [
-  {id: 0, name: 'Keywords', data_type: 'text', answer: '' },
+  {id: 0, name: 'Channel Keywords', data_type: 'text', answer: '' },
   {id: 1, name: 'Min. subscribers', data_type: 'number', answer: ''},
   {id: 2, name: 'Max. subscribers', data_type: 'number', answer: ''},
 ];
